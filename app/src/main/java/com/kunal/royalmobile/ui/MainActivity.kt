6f6d7a1a39cdb1b6c1daadc54a9a552eb37646a5
ashish8976 <113491231+ashish8976@users.noreply.github.com>
2022-12-10 13:52:52 +0530

    Packages created

--- app/src/main/java/com/kunal/royalmobile/ui/MainActivity.kt
+++ app/src/main/java/com/kunal/royalmobile/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.kunal.royalmobile
+package com.kunal.royalmobile.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kunal.royalmobile.R
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
     }
 }
